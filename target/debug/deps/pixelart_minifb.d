ab46ba954c4ef480f9ad26790a45281a9c9c65ce
Frank Loijenga <fjloijenga@gmail.com>
2023-05-04 00:50:22 +0200

    third try

--- target/debug/deps/pixelart_minifb.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Frank\Desktop\Code\Rust\pixelart\target\debug\deps\pixelart_minifb.exe: src\main.rs
-
-C:\Users\Frank\Desktop\Code\Rust\pixelart\target\debug\deps\pixelart_minifb.d: src\main.rs
-
-src\main.rs:
--- target/debug/pixelart_minifb.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Frank\Desktop\Code\Rust\pixelart\target\debug\pixelart_minifb.exe: C:\Users\Frank\Desktop\Code\Rust\pixelart\src\main.rs
